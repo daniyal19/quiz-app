4abefb775e635b8037fde3558f8e088f61187f97
Muhammad Daniyal Khan <57329937+daniyal19@users.noreply.github.com>
2025-05-13 17:21:17 +0500

    Add files via upload

---
+++ Quiz App/app.js
@@ -0,0 +1,95 @@
+var questions = [
+    {
+        question: "HTML Stands for",
+        option1: "Hyper Text Markup Language",
+        option2: "Hyper Tech Markup Language",
+        option3: "Hyper Touch Markup Language",
+        corrAnswer: "Hyper Text Markup Language",
+    },
+    {
+        question: "CSS Stands for",
+        option1: "Cascoding Style Sheets",
+        option2: "Cascading Style Sheets",
+        option3: "Cascating Style Sheets",
+        corrAnswer: "Cascading Style Sheets",
+    },
+    {
+        question: "Which tag is used for most large heading",
+        option1: "<h6>",
+        option2: "<h2>",
+        option3: "<h1>",
+        corrAnswer: "<h1>",
+    },
+    {
+        question: "Which tag is used to make element unique ",
+        option1: "id",
+        option2: "class  ",
+        option3: "label",
+        corrAnswer: "id",
+    },
+    {
+        question: "Any element assigned with id, can be get in css ",
+        option1: "by # tag",
+        option2: "by @ tag",
+        option3: "by & tag",
+        corrAnswer: "by # tag",
+    },
+    {
+        question: "CSS can be used with ______ methods ",
+        option1: "8",
+        option2: "3",
+        option3: "4",
+        corrAnswer: "3",
+    },
+    {
+        question: "In JS variable types are ____________ ",
+        option1: "6",
+        option2: "3",
+        option3: "8",
+        corrAnswer: "8",
+    },
+    {
+        question: "In array we can use key name and value ",
+        option1: "True",
+        option2: "False",
+        option3: "None of above",
+        corrAnswer: "False",
+    },
+    {
+        question: "toFixed() is used to define length of decimal ",
+        option1: "True",
+        option2: "False",
+        option3: "None of above",
+        corrAnswer: "True",
+    },
+    {
+        question: "push() method is used to add element in the start of array ",
+        option1: "True",
+        option2: "False",
+        option3: "None of above",
+        corrAnswer: "False",
+    },
+];
+
+var quesElement = document.getElementById("ques");
+var option1 = document.getElementById("opt1");
+var option2 = document.getElementById("opt2");
+var option3 = document.getElementById("opt3");
+var index = 0;
+
+function nextQuestion() {
+    if (index > questions.length - 1) {
+        console.log("question end..")
+    } else {
+        quesElement.innerText = questions[index].question;
+        option1.innerText = questions[index].option1;
+        option2.innerText = questions[index].option2;
+        option3.innerText = questions[index].option3;
+        index++;
+    }
+}
+
+function clicked() {
+    var nextBtn = document.getElementById("btn");
+    nextBtn.disabled = false;
+}
